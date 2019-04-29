63743aa84da9f87f4c9d1e1a29e7a5d424537223
ASLI ARI <asliari2012@gmail.com>
2019-04-30 00:02:03 +0200

    Add setData and Calculate funcs to mainpage

--- src/components/mainpage.vue
+++ src/components/mainpage.vue
@@ -14,8 +14,9 @@
       >Employer</span>
     </div>
     <keep-alive>
-      <component :is="activeComponent" v-on:setSalary="setDataemp($event)"/>
+      <component :is="activeComponent" v-on:setSalary="setDataemp($event, activeComponent)"/>
     </keep-alive>
+    <h1 class="modal-text">{{ msg }}</h1>
   </div>
 </template>
 
@@ -30,7 +31,8 @@ export default {
       employeeInput: null,
       employerInput: null,
       activeComponent: 'employee',
-      isActive: true
+      isActive: true,
+      msg: null
     }
   },
   components: { employee, employer },
@@ -38,11 +40,24 @@ export default {
     toggle: function () {
       this.isActive = !this.isActive
     },
-    setDataemp: function (data) {
+    setDataemp: function (data, activeComponent) {
       console.log(data)
+
+      if (activeComponent === 'employee') {
+        this.employeeInput = data.employeeInput
+      } else if (activeComponent === 'employer') {
+        this.employerInput = data.employerInput
+      }
     },
-    calculate () {
+    calculate (employeeInput, employerInput) {
       // calculate the data for succsess or failure
+      if (this.employeeInput <= this.employerInput) {
+        console.log('success!')
+        this.msg = 'success!';
+      } else {
+        console.log('Failure! Another condition')
+        this.msg = 'Failure!';
+      }
     }
   }
 }
