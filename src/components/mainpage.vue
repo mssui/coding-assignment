724b872a1ea4eda9e673b3d840d9b7b2ede54407
ASLI ARI <asliari2012@gmail.com>
2019-04-29 22:50:40 +0200

    Setup dynamic components as tabs

--- src/components/mainpage.vue
+++ src/components/mainpage.vue
@@ -1,15 +1,37 @@
 <template>
   <div class="hello">
     <h1>Main Page</h1>
+    <div class="tab">
+      <span
+        class="item"
+        :class="{ active: isActive }"
+        @click.prevent="activeComponent = 'employee'"
+      >Employee</span>
+      <span
+        class="item"
+        :class="{ active: !isActive }"
+        @click.prevent="activeComponent = 'employer'"
+      >Employer</span>
+    </div>
+    <keep-alive>
+      <component :is="activeComponent"/>
+    </keep-alive>
   </div>
 </template>
 
 <script>
+import employee from './employee.vue';
+import employer from './employer.vue';
+
 export default {
   name: 'mainpage',
   data () {
-    return {}
-  }
+    return {
+      activeComponent: 'employee',
+      isActive: true
+    }
+  },
+  components: { employee, employer }
 }
 </script>
 
